92e3676daec6f4de1d4d9584dda216ac92cb6a6b
Tomas Fernandez <tfernandez@mulesoft.com>
2021-05-13 18:34:31 -0300

    Initial commit: Spring starter + .gitignore

---
+++ src/test/java/org/austral/ingsis/compose/ComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.austral.ingsis.compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
